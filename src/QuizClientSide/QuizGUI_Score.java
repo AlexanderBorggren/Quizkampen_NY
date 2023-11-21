875272a346f2e2b154dafd6186321a6e909bd9bc
Alexander <146874712+AlexanderBorggren@users.noreply.github.com>
2023-11-21 01:34:42 +0100

    array update, TODO: skapa en transparent knapp som placeras ut först, if winner ändra knapp.

--- src/QuizClientSide/QuizGUI_Score.java
+++ src/QuizClientSide/QuizGUI_Score.java
@@ -4,23 +4,23 @@ import javax.swing.*;
 import java.awt.*;
 
 public class QuizGUI_Score {
-
     String playerName1 = "Player 1";
     String playerName2 = "Player 2";
     ImageIcon player1Icon = new ImageIcon("Images/Avatar_1.jpg");
     ImageIcon player2Icon = new ImageIcon("Images/Avatar_2.jpg");
 
     static int rounds = 10;
+    static int currentRound = 3;
     int scorePlayer1 = 0;
-
-    public static void setRounds(int rounds) {
-        QuizGUI_Score.rounds = rounds;
-    }
 
     int scorePlayer2 = 5;
     int roundPointsPlayer1 = 2;
     int roundPointsPlayer2 = 3;
     String whoTurn = "Your turn";
+
+    JPanel[] roundPanel = new JPanel[rounds];
+    JLabel[] winLabel = new JLabel[rounds];
+    JLabel[] loseLabel = new JLabel[rounds];
 
     QuizGUI_Score() {
         SwingUtilities.invokeLater(() -> {
@@ -85,33 +85,33 @@ public class QuizGUI_Score {
         return playerNamePanel;
     }
 
-    public static JPanel addScorePanel(int rounds, ImageIcon win, ImageIcon lose) {
+    public JPanel addScorePanel(int rounds, ImageIcon win, ImageIcon lose) {
         JPanel scorePanel = new JPanel();
         scorePanel.setLayout(new BoxLayout(scorePanel, BoxLayout.Y_AXIS));
         scorePanel.setBackground(Color.lightGray);
 
         for (int i = 0; i < rounds; i++) {
-            JPanel roundPanel = new JPanel(new FlowLayout());
-            roundPanel.setBackground(Color.lightGray);
-            JLabel winLabel = new JLabel(win);
-            JLabel loseLabel = new JLabel(lose);
-            roundPanel.add(winLabel);
-            roundPanel.add(loseLabel);
-            scorePanel.add(roundPanel);
-            /*if (isWinner(i)) {
-                winLabel.setVisible(true);
-                loseLabel.setVisible(false);
-            } else {
-                winLabel.setVisible(false);
-                loseLabel.setVisible(true);
-            }*/
+            roundPanel[i] = new JPanel(new FlowLayout());
+            roundPanel[i].setBackground(Color.lightGray);
+            winLabel[i] = new JLabel(win);
+            loseLabel[i] = new JLabel(lose);
+            roundPanel[i].add(winLabel[i]);
+            roundPanel[i].add(loseLabel[i]);
+            scorePanel.add(roundPanel[i]);
         }
+
         return scorePanel;
     }
 
-    /*public static boolean isWinner() {
-
-    }*/
+    public void showWinner(int round, boolean player1Won) {
+        if (player1Won) {
+            winLabel[round].setVisible(true);
+            loseLabel[round].setVisible(false);
+        } else {
+            loseLabel[round].setVisible(true);
+            winLabel[round].setVisible(false);
+        }
+    }
 
 
     public static void main(String[] args) {
