0080f9598998f7ae3d0057901aa47b5baf4f30b3
cezze_86 <cezze_86@hotmail.com>
2023-11-22 09:40:53 +0100

    Test

---
+++ src/DataBase/DatabaseQuestions.java
@@ -0,0 +1,32 @@
+package DataBase;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class DatabaseQuestions {
+    public static void main(String[] args) {
+
+        readFromFile("src/DataBase/Question");
+
+
+        readFromFile("src/DataBase/Answer");
+    }
+
+    private static void readFromFile(String fileName) {
+        ArrayList<String> data = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                data.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(fileName + ":"+"\n");
+        for (String line : data) {
+            System.out.println(line);
+        }
+        System.out.println();
+    }
+}
